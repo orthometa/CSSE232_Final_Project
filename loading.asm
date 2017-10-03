0afdc1a20a44e786cba2b8f04eb7ffa7ca6ee818
Ishan Saraf <sarafi@rose-hulman.edu>
2017-10-03 00:22:32 -0400

    Added sample program for loading from memory

---
+++ loading.asm
@@ -0,0 +1,11 @@
+#---------------------------------------------------------------
+# @author: Ishan Saraf, Rahul Yarlagadda, John McClure, David Li
+# @date: October 2nd, 2017
+#
+# This file contains a sample program to demonstrate loading
+# a value into memory from an address in our team's custom
+# assembly language.
+#---------------------------------------------------------------
+
+lw 0x1010   # Loads value at memory address 0x1010 and pushes it onto the stack
+            # Stack is 0(0x1010)
