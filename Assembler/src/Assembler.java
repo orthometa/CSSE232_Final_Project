bf59e495e5a06e6d6ff4934d3908c193cf58a735
Rahul Yarlagadda <knufire@gmail.com>
2017-10-10 00:56:57 -0400

    Initial commit of assembler in Java

---
+++ Assembler/src/Assembler.java
@@ -0,0 +1,138 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Assembler {
+	
+	
+	public static void main(String[] args) {
+		HashMap<String, Integer> labels = new HashMap<>();
+		//if (args.length == 0) return;
+		String fileName = "C:\\Users\\Rahul\\Google Drive\\CSSE232\\Project_Code\\Assembler\\src\\euclid.asm";
+		BufferedReader bufferedReader = null;
+		try {
+            // FileReader reads text files in the default encoding.
+            FileReader fileReader = new FileReader(fileName);
+            // Always wrap FileReader in BufferedReader.
+            bufferedReader = new BufferedReader(fileReader);
+		} catch (FileNotFoundException e) {
+			System.out.println("FILE NOT FOUND");
+			return;
+		}
+		ArrayList<String> lines = new ArrayList<>();
+		String line = null;
+		try {
+			while((line = bufferedReader.readLine()) != null) {
+			    //Remove comments
+				if(line.contains("#")) {
+					line = line.substring(0, line.indexOf('#'));
+				}
+				if (line.trim().length() > 0) {
+					lines.add(line);
+				}			
+			}
+		} catch (IOException e) {
+			System.out.println("IO ERROR");
+			return;
+		}
+		ArrayList<String> memory = new ArrayList<>();
+		//Find labels and store memory addresses
+		for (String ln : lines) {
+			
+			//Put labels in hashmap
+			if (ln.contains(":")) {
+				String[] split = ln.split(":");
+				labels.put(split[0], memory.size()*2);
+				ln = split[1];
+			}
+			
+			//For pushi, lw, sw, split the argument into it's own address
+			if (ln.contains("pushi") || ln.contains("lw") || ln.contains("sw")) {
+				String[] parts = ln.split("\\s+");
+				for (int i = 0; i < parts.length; i++) {
+					if (parts[i].trim().length() > 0) memory.add(parts[i]);
+				}
+			} else {
+				memory.add(ln);
+			}
+		}
+		StringBuilder program = new StringBuilder();
+		//Convert each memory address to machine code
+		for (int i = 0; i < memory.size(); i++) {
+			String[] words = memory.get(i).split("\\s+");
+			//Remove blank first word
+			if (words[0].isEmpty()) words = Arrays.copyOfRange(words, 1, words.length);
+			int instruction = 0;
+			if (Character.isDigit(words[0].charAt(0))) {
+				instruction = Integer.parseInt(words[0]);
+			}
+			else {
+				int opcode = getOpcode(words[0]);
+				if (opcode == 0) { //A-type
+					instruction = getFunc(words[0]);
+				} else if (opcode < 4) {//Branch instructions, B-type PC relative
+					int address = labels.get(words[1]); // Get address of branch destination
+					address = address - i*2;			// Subtract pc
+					address = address & 0x1FFF;			// Get bottom 13 bits of result
+					address = address >>> 1;			// Bit shift right once with zero extend
+					instruction = (opcode << 13) + address;
+				} else {//Jump instruction
+					int address = labels.get(words[1]);
+					address = address & 0x1FFF;			// Get bottom 13 bits of result
+					address = address >>> 1;			// Bit shift right once with zero extend
+					instruction = (opcode << 13) + address;
+				}
+			}
+			program.append(Integer.toHexString(0x10000 | i*2).substring(1).toUpperCase());
+			program.append(" ");
+			program.append(Integer.toHexString(0x10000 | instruction).substring(1).toUpperCase());
+			program.append("\n");
+		}
+		System.out.println(program.toString());
+		
+		
+	}
+	
+	
+	private static int getOpcode(String instr) {
+		switch(instr) {
+			case "beq": return 1;
+			case "bne": return 2;
+			case "bgt": return 3;
+			case "j": return 4;
+			case "jal": return 5;
+			default: return 0;
+		}
+	}
+	
+	private static int getFunc(String instr) {
+		switch(instr) {
+			case "add": return 1;
+			case "sub": return 2;
+			case "and": return 3;
+			case "or": return 4;
+			case "xor": return 5;
+			case "sll": return 6;
+			case "srl": return 7;
+			case "jr": return 8;
+			case "lw": return 9;
+			case "sw": return 10;
+			case "pushi": return 11;
+			case "dup": return 12; 
+			case "dup2": return 13;
+			case "pop": return 14;
+			case "in": return 15;
+			case "out": return 16;
+			case "disp": return 17;
+			case "swap": return 18;
+			default: throw new IllegalArgumentException();
+		}
+	}
+
+}
---
+++ Assembler/src/euclid.asm
@@ -0,0 +1,55 @@
+#---------------------------------------------------------------
+# @author: Ishan Saraf, Rahul Yarlagadda, John McClure, David pushi
+# @date: October 2nd, 2017
+#
+# This file contains Euclid's algorithm to find relative primes
+# as outlined on the CSSE232 final project page, written in our
+# team's custom assembly language.
+#---------------------------------------------------------------
+
+relP:		pushi 	2		#Push initial value of m
+				swap				#Swap, so stack is n->m, setup to call gcd
+
+while:	dup2				#Duplicate n and m, so they're still on the stack when gcd returns
+				dup2
+				jal gcd			#Call gcd
+				pushi 	1		#Load 1 onto the top of the stack
+				beq end			#Branch if result of gcd equals one
+				swap				#Didn't branch (so we're in the while loop)
+										#Swap so m is on top of stack and we can add 1 to m
+				pushi 	1
+				add					#Add 1 to m
+				swap				#Swap so stack is n->m (and m is incremented)
+				j 	while 	#Go back to beginning of while loop
+
+end:		pop					#pop n, so just m is on the stack
+				jr					#Return m
+
+gcd:		dup					#Duplicate a so we can consume it to branch
+				pushi 	0
+				beq next		#If a=0, go to the while loop.
+				pop					#Stack is a->b, so pop a and return b
+				jr
+
+next:		swap				#Swap so stack is b->a
+				dup					#Duplicate b so it's still on the stack after the branch
+				pushi 	0
+				beq exit		#Go to exit if b=0
+				dup2				#Duplicate a and b so stack is b->a->b->a
+				dup2
+				swap				#Swap for greater than
+				bgt cond2		#If a > b, go to the other condition
+				swap				#Stack is a->b
+				dup2				#Stack is b->a->b
+				swap				#Stack is a->b->b
+				sub					#a = a-b
+				j 	next		#Stack is back to a->b, go to beginning of loop
+
+cond2:	dup2				#Stack is now a->b-a
+				swap				#stack is now b->a->a
+				sub					#Stack is now (b-a)->a
+				swap				#Swap so b = b-a and stack is a->b
+				j 	next		#Go to beginning of while loop
+
+exit:		pop 				#Stack is currently b->a, pop b and return a
+				jr
